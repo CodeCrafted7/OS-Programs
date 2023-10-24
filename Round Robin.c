32275426071a22cc7e618a4ef2c3a49a314fbdad
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-10-24 12:02:00 +0530

    Update Round Robin.c

--- Round Robin.c
+++ Round Robin.c
@@ -1,82 +1,198 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#define MAX_SIZE 100
 
-void swap(int *x,int *y){
-    int temp=*x;
-    *x=*y;
-    *y=temp;
+struct process
+{
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int start_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+};
+
+int queue[MAX_SIZE];
+int front = -1;
+int rear = -1;
+
+void initializeQueue()
+{
+    front = -1;
+    rear = -1;
 }
 
-void sort(int at[], int bt[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (at[j] > at[j + 1]) {
-                swap(&at[j], &at[j + 1]);
-                swap(&bt[j], &bt[j + 1]);
+bool isFull()
+{
+    return (rear == MAX_SIZE - 1);
+}
+
+bool isEmpty()
+{
+    return (front == -1);
+}
+
+void push(int value)
+{
+    if (isFull())
+    {
+        printf("Queue is full. Cannot push %d.\n", value);
+        return;
+    }
+    if (isEmpty())
+        front = 0;
+    rear++;
+    queue[rear] = value;
+}
+
+void pop()
+{
+    if (isEmpty())
+    {
+        printf("Queue is empty. Cannot pop.\n");
+        return;
+    }
+    if (front == rear)
+        initializeQueue();
+    else
+        front++;
+}
+
+int getFront()
+{
+    if (isEmpty())
+    {
+        printf("Queue is empty.\n");
+        return -1;
+    }
+    return queue[front];
+}
+
+void bubbleSort(struct process p[], int n, int field)
+{
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if ((field == 0 ? p[j].arrival_time > p[j + 1].arrival_time : p[j].pid > p[j + 1].pid))
+            {
+                struct process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+}
+
+int main()
+{
+    int n,tq;
+    struct process p[100];
+    float avg_turnaround_time;
+    float avg_waiting_time;
+    float avg_response_time;
+    float cpu_utilisation;
+    float cpu_idle;
+    int total_turnaround_time = 0;
+    int total_waiting_time = 0;
+    int total_idle_time = 0;
+    float throughput;
+    int burst_remaining[100];
+    int idx;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    printf("time quantum: ");
+    scanf("%d", &tq);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter at and bt of process %d: ", i + 1);
+        scanf("%d %d", &p[i].arrival_time, &p[i].burst_time);
+        burst_remaining[i] = p[i].burst_time;
+        p[i].pid = i + 1;
+        printf("\n");
+    }
+    bubbleSort(p, n, 0);
+
+    initializeQueue();
+    push(0);
+    int current_time = 0;
+    int completed = 0;
+    int mark[100];
+    for (int i = 0; i < 100; i++)
+    {
+        mark[i] = 0;
+    }
+    mark[0] = 1;
+
+    while (completed != n)
+    {
+        idx = getFront();
+        pop();
+
+        if (burst_remaining[idx] == p[idx].burst_time)
+        {
+            p[idx].start_time = (current_time > p[idx].arrival_time) ? current_time : p[idx].arrival_time;
+            total_idle_time += p[idx].start_time - current_time;
+            current_time = p[idx].start_time;
+        }
+
+        if (burst_remaining[idx] - tq > 0)
+        {
+            burst_remaining[idx] -= tq;
+            current_time += tq;
+        }
+        else
+        {
+            current_time += burst_remaining[idx];
+            burst_remaining[idx] = 0;
+            completed++;
+
+            p[idx].completion_time = current_time;
+            p[idx].turnaround_time = p[idx].completion_time - p[idx].arrival_time;
+            p[idx].waiting_time = p[idx].turnaround_time - p[idx].burst_time;
+
+            total_turnaround_time += p[idx].turnaround_time;
+            total_waiting_time += p[idx].waiting_time;
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            if (burst_remaining[i] > 0 && p[i].arrival_time <= current_time && mark[i] == 0)
+            {
+                push(i);
+                mark[i] = 1;
             }
         }
-    }
-}
+        if (burst_remaining[idx] > 0)
+            push(idx);
 
-void count(int at[], int bt[], int ct[], int qt, int n) {
-    int remaningBT[n];
-    for (int i = 0; i < n; i++) {
-        remaningBT[i] = bt[i];
-    }
-    int time = 0;
-    if(at[0]!=0){
-        time +=at[0];
-    }
-    while (1) {
-        int done = 1;
-        for (int i = 0; i < n; i++) {
-            if (remaningBT[i] > 0) {
-                done = 0;
-                if (remaningBT[i] > qt) {
-                    time += qt;
-                    remaningBT[i] -= qt;
-                } else {
-                    time += remaningBT[i];
-                    ct[i] = time;
-                    remaningBT[i] = 0;
-                    ct[i]=time;
+        if (isEmpty())
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (burst_remaining[i] > 0)
+                {
+                    push(i);
+                    mark[i] = 1;
+                    break;
                 }
             }
         }
-        if (done == 1)
-            break;
     }
-}
-void tat_and_wt(int at[],int bt[],int ct[],int tat[],int wt[],int n){
-    for(int i=0;i<n;i++){
-        tat[i]=ct[i]-at[i];
-        wt[i]=tat[i]-bt[i];
-    }
-}
 
-void display(int at[],int bt[],int ct[],int tat[],int wt[],int n){
-    printf("AT\tBT\tCT\tTAT\tWT\n");
-    for(int i=0;i<n;i++){
-        printf("%d\t%d\t%d\t%d\t%d\n",at[i],bt[i],ct[i],tat[i],wt[i]);
-    }
-}
+    avg_turnaround_time = (float)total_turnaround_time / n;
+    avg_waiting_time = (float)total_waiting_time / n;
+    cpu_utilisation = ((p[n - 1].completion_time - total_idle_time) / (float)p[n - 1].completion_time) * 100;
+    throughput = (float)n / (p[n - 1].completion_time - p[0].arrival_time);
+    cpu_idle = (total_idle_time/(float)p[n-1].completion_time)*100;
+    bubbleSort(p, n, 1);
 
-int main(){
-    int n,qt;
-    printf("Enter the number of process: ");
-    scanf("%d",&n);
-    printf("Enter Assume time Quantum: ");
-    scanf("%d",&qt);
-    int at[n],bt[n],ct[n],tat[n],wt[n];
-    printf("\tAT\tBT\n");
-    for(int i=0;i<n;i++){
-        printf("p%d:",i+1);
-        scanf("%d %d",&at[i],&bt[i]);
-    }
-    sort(at,bt,n);
-    count(at,bt,ct,qt,n);
-    tat_and_wt(at,bt,ct,tat,wt,n);
-    display(at,bt,ct,tat,wt,n);
+    printf("id\tAT\tBT\tCT\tTAT\tWT\t\n");
+    for (int i = 0; i < n; i++)
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].arrival_time, p[i].burst_time,
+               p[i].completion_time, p[i].turnaround_time, p[i].waiting_time);
 
-    return 0;
+    printf("Avg TAT = %.2f\nAvg WT = %.2f\nCPU Utilization = %.2f%%\nCPU Idle = %.2f%%\nThroughput = %.2f processes/unit time\n",
+           avg_turnaround_time, avg_waiting_time, cpu_utilisation, cpu_idle, throughput);
 }
